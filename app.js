7aeba30aaa7afbe3de4b7603b2d2ca06d4fc90fc
Stepan Karslyan <stepan.karslyan@gmail.com>
2014-07-23 18:03:51 +0400

    adding d3js bar chart but not compleated

--- app.js
+++ app.js
@@ -5,12 +5,16 @@ app.listen(8989);
 
 app.use(express.static(__dirname + '/public'));
 app.use('/lib', express.static(__dirname + "/bower_components"));
-app.use(bodyParser());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(bodyParser.json());
 
-var statistics = [{taskName: "Test1", startDate: "12:30:30"},{taskName: "Test2", startDate: "12:36:45"}];
+//var statistics = [{taskName: "Test1", startDate: "12:30:30"},{taskName: "Test2", startDate: "12:36:45"}];
+//var dataArray = [45, 56, 61, 89];
 
 app.get("/statistics", function(req, res) {
-  res.send(JSON.stringify(statistics));
+  res.send(200);
 });
 
 console.log("Server running on port 8989");
--- public/js/statController.js
+++ public/js/statController.js
@@ -1,25 +1,17 @@
 angular.module("StatisticApp").controller("statController", function($scope) {
-
+   
   $.ajax({
     url: "/statistics",
     method: "GET",
     
     success: function(data) {
-      $scope.statistics = JSON.parse(data);
-        var array = [1,2,3,5];
-        d3.select("#chart").selectAll("div")
-        .data(array)
-        .enter()
-        .append("div")
-        .style("color", "red")
-        .text(function(d) {return d;});
-  
+//      $scope.statistics = JSON.parse(data);
+        buildChart(data);  
         console.log(data);
-      
-      
+           
       $scope.$apply();    
     }
   
   });
-
+  
 });
