a91178eb10ff6116881131ef2d65abb0790164f8
Stepan Karslyan <stepan.karslyan@gmail.com>
2014-07-25 14:11:44 +0400

    finishing ng_statistics

--- public/js/statController.js
+++ public/js/statController.js
@@ -5,25 +5,44 @@ angular.module("StatisticApp").controller("statController", function($scope) {
     $.ajax({
       url: "/calendarEvent",
       method: "GET",
-      data: { calendarId: "geqtfulmg33djpa049401p07oo@group.calendar.google.com",
-              tokens: {access_token:"ya29.TADESwIF_diGCEkAAACfT4IrZnFEU1w85154N5xphVz1APSPAizLKMo7OUbsNDYTLYJieRw9BzrNRxjJPE79Y0MbWEQ4q0DC5FPliboHfQg9ZWHpvhrHQdz73kGedQ",
-                      token_type:"Bearer",  
-                      expires_in :3599
+      data: { 
+        calendarId: "geqtfulmg33djpa049401p07oo@group.calendar.google.com",
+        tokens: {
+          access_token: "ya29.TQAxQpTcFEMHR0kAAACT02_ZtboFQ8hTrPNaG0sa19--vJLlE6j-9JppIs-ouwCG0XHPMtY8sXNwYQkiGG2shwbAnYDaomF0-hWzVYvKDxE9bHTlVjc0spd4olVp-g",
+          token_type: "Bearer",
+          expires_in: 3599
+        },
              
-              },
-              startDate: $scope.start + "T07:19:47.000Z",
-	            endDate: $scope.end + "T00:00:00.000Z"
-                
+        startDate: $scope.start + "T00:00:00.000Z",
+        endDate: $scope.end + "T00:00:00.000Z"
       },
+                
 
       success: function(data) {
-          
+        $scope.statistics = JSON.parse(data);
         console.log(data);  
+        buildChart($scope.statistics.tasks);
         $scope.$apply();    
-      },
+      }
       
 
     });
     
   };
+  
+  //chart function
+  
+  var buildChart = function(tasks) {
+    var bars = d3.select("#chart")                  
+            .selectAll("div.bar")
+            .data(tasks)
+            .enter().append("div")
+            .attr("class","bar")
+              .style("width",function(d) {return d.quantity * 10 + "px" })
+              .style("outline","0.5px solid grey") 
+              .text(function(d) { return d.quantity; });
+              
+   
+  };
+  
 });
