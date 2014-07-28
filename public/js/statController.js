226a22a9edc8b33f38afa6086c5ce08f9fefa2cb
Stepan Karslyan <stepan.karslyan@gmail.com>
2014-07-28 17:20:56 +0400

    trying d3js pie chart

--- public/js/statController.js
+++ public/js/statController.js
@@ -8,7 +8,7 @@ angular.module("StatisticApp").controller("statController", function($scope) {
       data: { 
         calendarId: "geqtfulmg33djpa049401p07oo@group.calendar.google.com",
         tokens: {
-          access_token: "ya29.TQAxQpTcFEMHR0kAAACT02_ZtboFQ8hTrPNaG0sa19--vJLlE6j-9JppIs-ouwCG0XHPMtY8sXNwYQkiGG2shwbAnYDaomF0-hWzVYvKDxE9bHTlVjc0spd4olVp-g",
+          access_token: "ya29.UABIJ2V5nT7u5kkAAAAR_9nkXGS4nSYnZkU0l59OuwaDuiBRHa2GlPAdIYBTVQoqfeKOI6A51jouR1P0fwBg06SWGDY7x_1IY5euLGIrGWv9iyxDR6fz2aJnD9b25g",
           token_type: "Bearer",
           expires_in: 3599
         },
@@ -20,8 +20,10 @@ angular.module("StatisticApp").controller("statController", function($scope) {
 
       success: function(data) {
         $scope.statistics = JSON.parse(data);
-        console.log(data);  
+        console.log(typeof($scope.statistics));
+        console.log($scope.statistics.tasks);  
         buildChart($scope.statistics.tasks);
+       // buildPieChart($scope.statistics.tasks)
         $scope.$apply();    
       }
       
@@ -29,20 +31,64 @@ angular.module("StatisticApp").controller("statController", function($scope) {
     });
     
   };
-  
+
   //chart function
   
-  var buildChart = function(tasks) {
-    var bars = d3.select("#chart")                  
-            .selectAll("div.bar")
-            .data(tasks)
-            .enter().append("div")
-            .attr("class","bar")
-              .style("width",function(d) {return d.quantity * 10 + "px" })
-              .style("outline","0.5px solid grey") 
-              .text(function(d) { return d.quantity; });
-              
-   
-  };
+  var buildChart = function(task) {
+    var div = d3.select("#chart");
+    
+    var bar = div.selectAll("div.bar")
+                  .data(task)
+                  .enter()
+                  .append("div")
+                  .attr("class", "bar")
+                  .style("background-color", function(d) {return d3.rgb(15, 240, + d.quantity * 3);})
+                  .style("height", function(d) {return d.quantity * 40 + "px"; })
+                  .text(function(d) {return d["name"];});
+                         
+  };          
+  
+  //pie chart
+  //var buildPieChart = function(tasks) {
+  
+    var data = [10, 20, 50];
+    var r = 200;
+
+    var color = d3.scale.ordinal()
+      .range(["red", "blue", "green"]);
+
+    var canvas = d3.select("#chart")
+      .append("svg")
+      .attr("width", "1500")
+      .attr("height", "1500");
+      
+    var group = canvas.append("g")
+      .attr("transform", "translate(300, 200)");
+      
+    var arc = d3.svg.arc()
+      .innerRadius(0)
+      .outerRadius(r);
+      
+    var pie = d3.layout.pie()
+      .value(function(d) {return d.data; });
+      
+    var arcs = group.selectAll(".arc")
+      .data(pie(data))
+      .enter()
+      .append("g")
+      .attr("class", "pie");
+      
+    arcs.append("path")
+      .attr("d", arc)
+      .attr("fill", function(d) { return color(d.data); });
+      
+    arcs.append("text")
+      .attr("transform", function(d) { return "translate(" + arc.centroid(d) + ")"; })
+      .attr("text-anchor", "middle")
+      .attr("font-size", "1.5em")
+      .text(function(d) { return d.data; });
+    
+  
+ // };
   
 });
